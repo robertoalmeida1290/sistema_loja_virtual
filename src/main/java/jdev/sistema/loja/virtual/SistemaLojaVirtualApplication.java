37a8bfa6b55ff45ae4ad0b25bdbd47be02622de9
roberto <roberto@AB007135R.usinaabe.local>
2023-08-20 21:20:02 -0300

    data 20/08/2023

---
+++ src/main/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package jdev.sistema.loja.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLojaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.sistema.loja.virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
