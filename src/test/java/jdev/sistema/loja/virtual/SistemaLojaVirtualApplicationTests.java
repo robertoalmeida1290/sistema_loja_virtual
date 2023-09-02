9d6eda9f20b52dd5dfac2c94521cc2284ce9dda4
roberto <roberto@AB007135R.usinaabe.local>
2023-09-02 19:39:15 -0300

    acertado ate o meven

--- src/test/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplicationTests.java
+++ src/test/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import jdev.sistema.loja.virtual.controller.AcessoController;
 import jdev.sistema.loja.virtual.model.Acesso;
 import jdev.sistema.loja.virtual.repository.AcessoRepository;
 import jdev.sistema.loja.virtual.service.AcessoService;
@@ -18,20 +19,34 @@ public class SistemaLojaVirtualApplicationTests {
 	@Autowired
 	private AcessoService acessoService;
 	
-	///@Autowired
+	@Autowired
+	private AcessoController acessoController;
+	
 	///private AcessoRepository acessoRepository;
 	
+	
+	/*esta salvando pelo dois medotodo,,, service e controller*/
 
 	@Test
 	 public void testCadastroAcesso() {
 		
 		Acesso acesso = new Acesso();
-		acesso.setDescricao("teste de acesso 2");
+		acesso.setDescricao("teste de acesso service 1");
 		
+
 		acessoService.save(acesso);
-	
+		}
+	@Test
+	 public void testCadastroAcesso2() {
+		
+		Acesso acesso = new Acesso();
+		acesso.setDescricao("teste de acesso controller 1 ");
+		
+
+		acessoController.salvarAcesso(acesso);
 		}
 }
+
 
 
 /*
