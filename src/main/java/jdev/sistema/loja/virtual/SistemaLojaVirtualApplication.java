55897478a3c1c58e706a2e8826efa692c732eff2
roberto <roberto@AB007135R.usinaabe.local>
2023-09-02 11:10:24 -0300

    dia 02/09/2023 tudo certo ate aqui,,,

--- src/main/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplication.java
+++ src/main/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplication.java
@@ -2,8 +2,16 @@ package jdev.sistema.loja.virtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "jdev.sistema.loja.virtual.model")
+@ComponentScan(basePackages = {"jdev.*"})
+@EnableJpaRepositories(basePackages = {"jdev.sistema.loja.virtual.repository"})
+@EnableTransactionManagement
 public class SistemaLojaVirtualApplication {
 
 	public static void main(String[] args) {
