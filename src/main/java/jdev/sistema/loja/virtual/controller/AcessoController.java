af6f4ff674687d958b5066c6bd94c61b862ce85b
roberto <roberto@AB007135R.usinaabe.local>
2023-09-03 20:25:41 -0300

    dia 03092023 20 25

--- src/main/java/jdev/sistema/loja/virtual/controller/AcessoController.java
+++ src/main/java/jdev/sistema/loja/virtual/controller/AcessoController.java
@@ -1,23 +1,49 @@
 package jdev.sistema.loja.virtual.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import jdev.sistema.loja.virtual.model.Acesso;
+import jdev.sistema.loja.virtual.repository.AcessoRepository;
 import jdev.sistema.loja.virtual.service.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 	
 	@Autowired
 	private AcessoService acessoService;
 	
+	@Autowired
+	private AcessoRepository acessoRepository;
 	
-	@PostMapping("/salvarAcesso")
-	public Acesso salvarAcesso(Acesso acesso) {
+	@ResponseBody /*Poder dar um retorno da API*/
+	@PostMapping(value = "**/salvarAcesso") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) { /*Recebe o JSON e converte pra Objeto*/
 		
-		return acessoService.save(acesso);
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+	}
+	
+	
+	@ResponseBody /*Poder dar um retorno da API*/
+	@PostMapping(value = "**/deleteAcesso") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) { /*Recebe o JSON e converte pra Objeto*/
+		
+		acessoRepository.deleteById(acesso.getId());
+			
+		return new ResponseEntity("Acesso Removido",HttpStatus.OK);
 	}
 
 }
+
+	/*{"id":"", "descricao": "ROLE_TESTEPOSTMAM"}*/
+	/*localhost:8080/sistema_loja_virtual/salvarAcesso*/
+	
--- src/test/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplicationTests.java
+++ src/test/java/jdev/sistema/loja/virtual/SistemaLojaVirtualApplicationTests.java
@@ -31,7 +31,7 @@ public class SistemaLojaVirtualApplicationTests {
 	 public void testCadastroAcesso() {
 		
 		Acesso acesso = new Acesso();
-		acesso.setDescricao("teste de acesso service 1");
+		acesso.setDescricao("teste de acesso service domingo");
 		
 
 		acessoService.save(acesso);
@@ -40,50 +40,9 @@ public class SistemaLojaVirtualApplicationTests {
 	 public void testCadastroAcesso2() {
 		
 		Acesso acesso = new Acesso();
-		acesso.setDescricao("teste de acesso controller 1 ");
+		acesso.setDescricao("teste de acesso controller domingo ");
 		
 
 		acessoController.salvarAcesso(acesso);
 		}
 }
-
-
-
-/*
-package jdev.sistema.loja.virtual;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import jdev.sistema.loja.virtual.model.Acesso;
-import jdev.sistema.loja.virtual.controller.AcessoController;
-import jdev.sistema.loja.virtual.repository.AcessoRepository;
-import jdev.sistema.loja.virtual.service.AcessoService;
-
-
-@SpringBootTest(classes = SistemaLojaVirtualApplicationTests.class)
-public class SistemaLojaVirtualApplicationTests {
-	
-	@Autowired
-	private AcessoController acessoController;
-	
-	@Autowired
-	private AcessoService acessoService;
-	
-	@Autowired
-	private AcessoRepository acessoRepository;
-		
-	@Test
-	public void testCadastraAcesso() {
-		
-		Acesso acesso = new Acesso();
-		
-		acesso.setDescricao("ROLE_ADMIN");
-
-		acessoRepository.save(acesso);
-	
-	}
-
-}
-*/
