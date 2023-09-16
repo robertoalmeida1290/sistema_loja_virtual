fe65b956ecee30494bc0254dfb2fe90bdf2ceaf6
roberto <roberto@AB007135R.usinaabe.local>
2023-09-16 08:09:36 -0300

    Sabado 16062023

--- src/main/java/jdev/sistema/loja/virtual/controller/AcessoController.java
+++ src/main/java/jdev/sistema/loja/virtual/controller/AcessoController.java
@@ -1,9 +1,12 @@
 package jdev.sistema.loja.virtual.controller;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,17 +36,48 @@ public class AcessoController {
 	}
 	
 	
-	@ResponseBody /*Pode dar um retorno da API*/
+	
+	@ResponseBody /*Poder dar um retorno da API*/
 	@PostMapping(value = "**/deleteAcesso") /*Mapeando a url para receber JSON*/
 	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) { /*Recebe o JSON e converte pra Objeto*/
 		
 		acessoRepository.deleteById(acesso.getId());
-			
+		
 		return new ResponseEntity("Acesso Removido",HttpStatus.OK);
 	}
+	
+	@ResponseBody
+	@DeleteMapping(value = "**/deleteAcessoPorId/{id}")
+	public ResponseEntity<?> deleteAcessoPorId(@PathVariable("id") Long id) { 
+		
+		acessoRepository.deleteById(id);
+		
+		return new ResponseEntity("Acesso Removido",HttpStatus.OK);
+	}
+	
+	
+	
+	@ResponseBody
+	@GetMapping(value = "**/obterAcesso/{id}")
+	public ResponseEntity<Acesso> obterAcesso(@PathVariable("id") Long id) { 
+		
+		Acesso acesso = acessoRepository.findById(id).get();
+		
+		return new ResponseEntity<Acesso>(acesso,HttpStatus.OK);
+	}
+	
+	
+	
+	@ResponseBody
+	@GetMapping(value = "**/buscarPorDesc/{desc}")
+	public ResponseEntity<List<Acesso>> buscarPorDesc(@PathVariable("desc") String desc) { 
+		
+		List<Acesso> acesso = acessoRepository.buscarAcessoDesc(desc);
+		
+		return new ResponseEntity<List<Acesso>>(acesso,HttpStatus.OK);
+	}
+	
+	
+	
 
 }
-
-	/*{"id":"", "descricao": "ROLE_TESTEPOSTMAM"}*/
-	/*localhost:8080/sistema_loja_virtual/salvarAcesso*/
-	
